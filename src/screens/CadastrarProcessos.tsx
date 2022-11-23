4ded178d3c53a6b6d92f9bd7a66a8c10ba62d379
zevit0r <z3.vit07@gmail.com>
2022-11-23 11:37:49 -0300

    add register of process

--- src/screens/CadastrarProcessos.tsx
+++ src/screens/CadastrarProcessos.tsx
@@ -1,10 +1,10 @@
 import { StyleSheet, View, Dimensions } from 'react-native';
-import { Text, Button } from '@rneui/base';
+import { Text, Button, Input } from '@rneui/base';
 import { useState, useContext  } from 'react';
 import AuthContext from '../contexts/auth';
 import {getProcessoId} from '../service/processos.service';
 import {api} from "../service/api";
-
+import axios from 'axios';
 
 const screen = Dimensions.get("screen");
 
@@ -14,23 +14,77 @@ const vw = screen.width;
 export default function CadastrarProcessos() {
 
     const {signOut} = useContext(AuthContext)
+    const [ numberProcess, setNumberProcess ] = useState("");
+    const [ partsProcess, setPartsProcess ] = useState("");
+    const [ reporterProcess, setReporterProcess ] = useState("");
+    const [ resumeProcess, setResumeProcess ] = useState("");
 
+    async function registerProcess() {
+        if(numberProcess == ''){
+            alert('Preenche o número do processo')
+            return 
+        }
+        else if(partsProcess == ''){
+            alert('Preenche as partes do processo')
+            return 
+        }
+        else if(reporterProcess == ''){
+            alert('Preenche o relator do processo')
+            return 
+        }
+        else if(resumeProcess == ''){
+            alert('Preenche o resumo do processo')
+            return 
+        }
 
-   
-
-    function clickSair(){
-        signOut();
-        // getTest()
+        const process = {
+            numero: numberProcess,
+            partes: partsProcess,
+            relator: reporterProcess,
+            resumo: resumeProcess
+        }
+        
+        axios.post('/processos',{...process})
+            .then(function (response) {
+                alert(response);
+            })
+            .catch(function (error) {
+                alert(error);
+            });
     }
 
     return (
 
         <View style={styles.container}>
+            <View style = {styles.dataProcess}>
+                <Text style={styles.text}>Pauta: </Text>
+            </View>
+            <View style = {styles.dataProcess}>
+                <Text style={styles.text}>Dados do Processo:</Text>
+                <Input style={styles.textInput}
+                    placeholder='Número do processo'
+                    onChangeText={value => setNumberProcess(value)}
 
-            <Text>Cadastar Processos</Text>
-            <Button title='Sair'
-            onPress={clickSair}
-            />
+                    />
+                <Input style={styles.textInput}
+                    placeholder='Partes do processo'
+                    onChangeText={value => setPartsProcess(value)}
+
+                    />
+                <Input style={styles.textInput}
+                    placeholder='Relator do processo'
+                    onChangeText={value => setReporterProcess(value)}
+
+                    />
+                <Input style={styles.textInput}
+                    placeholder='Resumo do processo'
+                    onChangeText={value => setResumeProcess(value)}
+
+                    />
+
+                <Button title='Cadastrar' style={styles.registerProcess} onPress={registerProcess}></Button>
+            </View>
+
         </View>
     )
 
@@ -40,12 +94,35 @@ const styles = StyleSheet.create({
     container: {
       margin: 0,
       padding: 0,
+      paddingBottom: 20,
       height: vh,
       width: vw,
       flex: 1,
       flexDirection: 'column',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'space-evenly',
       backgroundColor: '#fff',
     },
+    textInput: {
+        margin: 0,
+        padding: 0,
+        width: vw,
+    },
+    text: {
+        margin: 8,
+        padding: 0,
+        width: vw,
+        color: "#01426A",
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    dataProcess: {
+        margin: 0,
+        padding: 0,
+        width: vw,
+    },
+    registerProcess: {
+        marginLeft: 10,
+        marginRight: 10,
+    }
 });  
